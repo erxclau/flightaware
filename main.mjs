7da96eb36883c8c15d44806793fe02270bc40760
erxclau <ericlau41802@gmail.com>
2025-11-06 20:59:54 -0500

    Add script

---
+++ main.mjs
@@ -0,0 +1,71 @@
+import { csvFormat, tsvParse } from "d3-dsv";
+import { JSDOM } from "jsdom";
+import { readFileSync, writeFileSync } from "node:fs";
+import { exit } from "node:process";
+
+const airports = tsvParse(readFileSync("./airports.tsv").toString());
+
+const weekday = {
+  "https://www.flightaware.com/live/cancelled/minus3days/": "Monday",
+  "https://www.flightaware.com/live/cancelled/minus2days/": "Tuesday",
+  "https://www.flightaware.com/live/cancelled/yesterday/": "Wednesday",
+  "https://www.flightaware.com/live/cancelled/today/": "Thursday",
+};
+
+const sleep = async (ms) => {
+  return new Promise((res) => setTimeout(res, ms));
+};
+
+const rows = [];
+
+for await (const airport of [{ icao: "", name: "All" }, ...airports]) {
+  const urlBases = [
+    "https://www.flightaware.com/live/cancelled/minus3days/",
+    "https://www.flightaware.com/live/cancelled/minus2days/",
+    "https://www.flightaware.com/live/cancelled/yesterday/",
+    "https://www.flightaware.com/live/cancelled/today/",
+  ];
+
+  for await (const urlBase of urlBases) {
+    const url = new URL(airport.icao, urlBase);
+
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+
+    const html = await response.text();
+    const dom = new JSDOM(html);
+
+    const [
+      _,
+      totalDelays,
+      totalUsDelays,
+      totalCancellations,
+      totalUsCancellations,
+    ] = Array.from(dom.window.document.querySelectorAll("h3")).map((d) => {
+      const content = d.textContent;
+      return +content
+        .substring(content.lastIndexOf(":") + 1)
+        .trim()
+        .replaceAll(",", "");
+    });
+
+    const row = {
+      ...airport,
+      day: weekday[urlBase],
+      totalDelays,
+      totalUsDelays,
+      totalCancellations,
+      totalUsCancellations,
+    };
+
+		console.log(row)
+
+    rows.push(row);
+
+    await sleep(1000);
+  }
+}
+
+writeFileSync("./stats.csv", csvFormat(rows));
